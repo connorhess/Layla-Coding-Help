07f36fe3bf7307462d649d37c6b79e3cc7ca03b1
Connor <hessconnor41@gmail.com>
2023-10-26 11:04:11 +0200

    added user input

--- 10-028 Recursion/sum_recursion.py
+++ 10-028 Recursion/sum_recursion.py
@@ -1,7 +1,18 @@
+import ast
+
 def adding_up_to(lst, index):
     if index == 0:
         return lst[0]
     else:
         return lst[index] + adding_up_to(lst, index-1)
 
-print(adding_up_to([1,4,5,3,12,16], 4))
+
+
+lst_str = input("Enter a list of numbers: ")
+lst = ast.literal_eval(lst_str)
+
+index_str = input("Enter an index: ")
+index = int(index_str)
+
+result = adding_up_to(lst, index)
+print(result)
